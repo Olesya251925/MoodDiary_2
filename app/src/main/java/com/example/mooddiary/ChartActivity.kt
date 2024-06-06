3bd1d3f44991b122679ed12e507982c087324b31
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2024-06-06 12:59:01 +0700

    Update ChartActivity.kt

--- app/src/main/java/com/example/mooddiary/ChartActivity.kt
+++ app/src/main/java/com/example/mooddiary/ChartActivity.kt
@@ -19,41 +19,49 @@ class ChartActivity : AppCompatActivity() {
         binding = ActivityChartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Инициализируем ViewModel для взаимодействия с данными настроений
         moodViewModel = ViewModelProvider(this).get(MoodViewModel::class.java)
 
+        // Получаем ссылки на элементы интерфейса
         val startDateEditText = binding.editTextStartDate
         val endDateEditText = binding.editTextEndDate
         val buttonShowChart = binding.buttonShowChart
 
-        // Обработчики кликов для полей ввода дат
+        // Обработчики кликов для полей ввода дат, показывающие диалог выбора даты
         startDateEditText.setOnClickListener { showDatePickerDialog(startDateEditText) }
         endDateEditText.setOnClickListener { showDatePickerDialog(endDateEditText) }
 
         // Обработчик клика для кнопки "Показать график"
         buttonShowChart.setOnClickListener {
+            // Получаем и парсим даты из полей ввода
             val startDate = dateFormat.parse(startDateEditText.text.toString())
             val endDate = dateFormat.parse(endDateEditText.text.toString())
+            // Если даты корректны, фильтруем и отображаем данные настроений
             if (startDate != null && endDate != null) {
                 filterAndDisplayMoods(startDate, endDate)
             }
         }
 
+        // Наблюдаем за данными настроений и отображаем их на графике
         // Получаем данные из базы данных и строим график
         moodViewModel.allMoods.observe(this, { moodEntries: List<MoodEntry>? ->
             moodEntries?.let {
+                // Приводим canvas к типу ChartView и устанавливаем данные настроений
                 val chartView = binding.canvas as ChartView
                 chartView.setMoodEntries(it)
             }
         })
     }
 
-    //календарь
+    // Показывает диалог выбора даты и устанавливает выбранную дату в EditText
     private fun showDatePickerDialog(editText: EditText) {
         val calendar = Calendar.getInstance()
         val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            // Устанавливаем выбранную дату в календарь
             calendar.set(Calendar.YEAR, year)
             calendar.set(Calendar.MONTH, month)
             calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            // Форматируем дату и устанавливаем в EditText
             editText.setText(dateFormat.format(calendar.time))
         }
 
@@ -64,6 +72,7 @@ class ChartActivity : AppCompatActivity() {
         config.setLocale(locale)
         createConfigurationContext(config)
 
+        // Показываем диалог выбора даты с текущей датой по умолчанию
         DatePickerDialog(
             this,
             dateSetListener,
@@ -73,13 +82,20 @@ class ChartActivity : AppCompatActivity() {
         ).show()
     }
 
-
+    // Фильтрует и отображает записи настроений в заданном диапазоне дат
     private fun filterAndDisplayMoods(startDate: Date, endDate: Date) {
+        // Фильтруем данные настроений по диапазону дат
         val filteredMoods = moodViewModel.allMoods.value?.filter {
+            // Преобразуем строку даты из MoodEntry в объект Date
             val date = dateFormat.parse(it.date)
+            // Проверяем, находится ли дата в заданном диапазоне
             date != null && date >= startDate && date <= endDate
         }
+
+        // Приводим canvas к типу ChartView и устанавливаем отфильтрованные данные настроений
         val chartView = binding.canvas as ChartView
+        // Если filteredMoods не является null, устанавливаем его как данные для отображения. В противном случае, устанавливаем пустой список.
         chartView.setMoodEntries(filteredMoods ?: listOf())
     }
+
 }
