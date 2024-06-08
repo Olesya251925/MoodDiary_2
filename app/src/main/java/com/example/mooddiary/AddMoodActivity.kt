573e19390e4af1067f259d51260493516508ce4b
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2024-06-08 16:42:30 +0700

    Update AddMoodActivity.kt

--- app/src/main/java/com/example/mooddiary/AddMoodActivity.kt
+++ app/src/main/java/com/example/mooddiary/AddMoodActivity.kt
@@ -1,24 +1,26 @@
 package com.example.mooddiary
 
-import android.content.Intent
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.*
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.util.*
 
 class AddMoodActivity : AppCompatActivity() {
 
     private val moodViewModel: MoodViewModel by viewModels()
+    private lateinit var editTextDate: EditText
+    private lateinit var editTextTime: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_mood)
 
-        val editTextDate: EditText = findViewById(R.id.editTextDate)
-        val editTextTime: EditText = findViewById(R.id.editTextTime)
+        editTextDate = findViewById(R.id.editTextDate)
+        editTextTime = findViewById(R.id.editTextTime)
         val editTextDescription: EditText = findViewById(R.id.editTextDescription)
         val spinnerMood: Spinner = findViewById(R.id.spinnerMood)
         val buttonSave: Button = findViewById(R.id.buttonSave)
@@ -30,51 +32,86 @@ class AddMoodActivity : AppCompatActivity() {
         val modeType = intent.getStringExtra("modeType") ?: "Add"
         val isNewNote = modeType == "Add"
         var id = 0
-        var oldMoodEntry: MoodEntry? = null
 
         if (!isNewNote) {
-            // Если это не новая запись (редактирование существующей).
-            id = intent.getIntExtra("id", 0) // Из Intent извлекается ID записи настроения.
+            id = intent.getIntExtra("id", 0)
             editTextDate.setText(intent.getStringExtra("date"))
             editTextTime.setText(intent.getStringExtra("time"))
             editTextDescription.setText(intent.getStringExtra("description"))
             val mood = intent.getStringExtra("mood")
             val spinnerPosition = adapter.getPosition(mood)
             spinnerMood.setSelection(spinnerPosition)
-
-            //Intent позволяет передать все необходимые данные из одной активности в другую, чтобы пользователь мог
-           // редактировать существующую запись настроения, видя все текущие данные этой записи.
         }
 
-        // Устанавливаем слушатель для кнопки "Сохранить".
+        editTextDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        editTextTime.setOnClickListener {
+            showTimePickerDialog()
+        }
+
         buttonSave.setOnClickListener {
             val date = editTextDate.text.toString()
             val time = editTextTime.text.toString()
             val mood = spinnerMood.selectedItem.toString()
             val description = editTextDescription.text.toString()
 
-            //если создние
             if (isNewNote) {
-                // Создаём новый объект MoodEntry с полученными данными.
                 val newMoodEntry = MoodEntry(date = date, time = time, mood = mood, description = description, status = " ")
-                //вставляем в бд
                 moodViewModel.insert(newMoodEntry)
                 Toast.makeText(this, "Данные добавлены", Toast.LENGTH_SHORT).show()
             } else {
-                // Если это редактирование существующей записи.
                 val updatedMoodEntry = MoodEntry(
                     id = id,
                     date = date,
                     time = time,
                     mood = mood,
                     description = description,
-                    // Обновляем статус записи с текущей датой и временем.
-                    status = "Обновлено:  ${SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format( Date() )}"
+                    status = "Обновлено: ${SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format(Date())}"
                 )
                 moodViewModel.update(updatedMoodEntry)
                 Toast.makeText(this, "Данные изменены", Toast.LENGTH_SHORT).show()
             }
             finish()
         }
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+
+        // Устанавливаем локаль на русский
+        val locale = Locale("ru")
+        Locale.setDefault(locale)
+        val config = resources.configuration
+        config.setLocale(locale)
+        createConfigurationContext(config)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                val selectedDate = String.format("%02d.%02d.%d", dayOfMonth, month + 1, year)
+                editTextDate.setText(selectedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
+    private fun showTimePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val timePickerDialog = TimePickerDialog(
+            this,
+            { _, hourOfDay, minute ->
+                val selectedTime = String.format("%02d:%02d", hourOfDay, minute)
+                editTextTime.setText(selectedTime)
+            },
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            true
+        )
+        timePickerDialog.show()
     }
 }
